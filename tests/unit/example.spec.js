87f5ac804ff03942a3f308ff8f68e9fbfc5ac18d
Donghyun Kim <dhx515.official@gmail.com>
2024-12-11 00:02:19 +0900

    init: todo_orchestrator 프로젝트 생성

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,6 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("test1", () => {
+  it("1=1", () => {
+    expect(1).toBe(1);
   });
 });
