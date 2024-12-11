072e759e42e41c1ae24bbead28f9045e64a802c7
Donghyun Kim <dhx515.official@gmail.com>
2024-12-11 21:59:54 +0900

    Test: logic, DoneDataPipeline 테스트 코드 추가

---
+++ tests/unit/DoneDataPipeline.spec.js
@@ -0,0 +1,31 @@
+import { DoneDataPipelineConfig } from '@/logic/core/task/pipelines/done/config';
+
+describe('DoneDataPipeline', () => {
+    let doneDataPipeline;
+
+    beforeEach(async () => {
+        doneDataPipeline = DoneDataPipelineConfig();
+        await doneDataPipeline.command('loadData', {1:2});
+    });
+
+    it('loadData', async() => {
+        const result = await doneDataPipeline.command('loadData', {1:2});
+        expect(result).toStrictEqual(['분기계획작성', '운영인수인계', '화상회의']);
+    });
+
+    it('createLoad', async() => {
+        const result = await doneDataPipeline.command('createLoad', '커피챗');
+        expect(result).toStrictEqual(['분기계획작성', '운영인수인계', '화상회의', '커피챗']);
+    });
+
+    it('deleteLoad', async() => {
+        const result = await doneDataPipeline.command('deleteLoad', '분기계획작성');
+        expect(result).toStrictEqual(['운영인수인계', '화상회의']);
+    });
+
+    it('createLoad and deleteLoad', async() => {
+        await doneDataPipeline.command('createLoad', '커피챗');
+        const result = await doneDataPipeline.command('deleteLoad', '운영인수인계');
+        expect(result).toStrictEqual(['분기계획작성', '화상회의', '커피챗']);
+    });
+});
