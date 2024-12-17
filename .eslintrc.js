0befe49c59737118d9212edc4915ba7837d54b1c
Donghyun Kim <dhx515.official@gmail.com>
2024-12-18 00:47:07 +0900

    Chore: eslint 설정 오류 수정

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// .eslintrc.js 예시
+module.exports = {
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+    rules: {
+      // 여기에 extensions 옵션이 없는지 확인하세요.
+    }
+};
--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,10 @@ module.exports = defineConfig({
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
+  
+  chainWebpack: config => {
+    config.plugin('eslint').tap(args => {
+      return args; // extensions 옵션을 제거
+    });
+  },
 });
