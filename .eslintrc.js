7cfa17743f42ae822d64fdcc2274c2715063ffb3
Donghyun Kim <dhx515.official@gmail.com>
2024-12-22 16:05:50 +0900

    Chore: eslint 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
-// .eslintrc.js 예시
 module.exports = {
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
-    rules: {
-      // 여기에 extensions 옵션이 없는지 확인하세요.
-    }
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module'
+  },
+  rules: {
+      'vue/multi-word-component-names': 'off', // 단일 단어 컴포넌트 이름 허용
+      'vue/require-default-prop': 'off',       // 기본 Prop 강제 비활성화
+      'no-unused-vars': ['error', { 
+            args: 'none', // 함수/메서드 파라미터를 무시
+            vars: 'all'   // 변수는 여전히 체크
+        }]
+  }
 };
