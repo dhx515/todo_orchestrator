7a1d56a9d2e6fa3851424601a82dbfa04db4672c
Donghyun Kim <dhx515.official@gmail.com>
2024-12-23 23:39:35 +0900

    Chore: eslint 들여쓰기 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   parserOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module'
+    ecmaVersion: 2022,
+    sourceType: 'module'
   },
   rules: {
-      'vue/multi-word-component-names': 'off', // 단일 단어 컴포넌트 이름 허용
-      'vue/require-default-prop': 'off',       // 기본 Prop 강제 비활성화
-      'no-unused-vars': ['error', { 
-            args: 'none', // 함수/메서드 파라미터를 무시
-            vars: 'all'   // 변수는 여전히 체크
-        }]
+    'vue/multi-word-component-names': 'off', // 단일 단어 컴포넌트 이름 허용
+    'vue/require-default-prop': 'off',       // 기본 Prop 강제 비활성화
+    'no-unused-vars': ['error', { 
+      args: 'none', // 함수/메서드 파라미터를 무시
+      vars: 'all'   // 변수는 여전히 체크
+    }]
   }
 };
